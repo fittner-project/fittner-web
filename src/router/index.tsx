76dc096f52d054242e13bc570151fc32f6f57bcb
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-02 21:15:47 +0900

    [CREATE] 로그인 페이지 라우터 연결

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,27 +1,32 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import RootLayout from '@/layout/default.tsx'
-import About from '@/views/About'
-import Home from '@/views/Home'
+import RootLayout from "@/layout/default.tsx";
+import About from "@/views/About";
+import Home from "@/views/Home";
+import SignIn from "@/views/SignIn";
 
 const router = createBrowserRouter(
   [
     {
-      path: '',
+      path: "",
       element: <RootLayout />,
       children: [
         {
-          path: '',
+          path: "",
           element: <Home />,
         },
         {
-          path: '/about',
+          path: "/about",
           element: <About />,
+        },
+        {
+          path: "/sign-in",
+          element: <SignIn />,
         },
       ],
     },
   ],
-  { basename: import.meta.env.VITE_PUBLIC_PATH as string },
-)
+  { basename: import.meta.env.VITE_PUBLIC_PATH as string }
+);
 
-export default router
+export default router;
