1e514babb300358f8a977e9867d78f33a385ea12
youngjae <newy12@naver.com>
2025-01-05 23:31:09 +0900

    #test. 파이프라인 webhook test

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import { useVcosole } from '@/hooks/useVconsole'
 // 这个是全局的页面 还可以做一些其他的操作
 
+//파이프라인 배포테스트
 export default function RootLayout() {
   const [vc] = useVcosole()
   useEffect(() => {
