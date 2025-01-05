d21c1629a0eff75224f5175ae55b345ba5a459d8
youngjae <newy12@naver.com>
2025-01-06 00:23:40 +0900

    #fix. 랜더링 이슈 수정

--- src/hooks/useVconsole.ts
+++ src/hooks/useVconsole.ts
@@ -1,8 +1,21 @@
-import VConsole from 'vconsole'
-export function useVcosole() {
-  const vc = useRef<VConsole | null>(null)
-  if (location.href.includes('#vc')) {
-    vc.current = new VConsole({ theme: 'dark', maxLogNumber: 1000 })
-  }
-  return [vc.current] as const
+import { useState, useEffect, useRef } from 'react';
+import VConsole from 'vconsole';
+
+export function useVconsole() {
+  const [isClient, setIsClient] = useState(false);  // 클라이언트 여부 확인
+  const vc = useRef<VConsole | null>(null);
+
+  // 클라이언트에서만 실행되도록 설정
+  useEffect(() => {
+    setIsClient(true);  // 클라이언트에서만 실행
+  }, []);
+
+  useEffect(() => {
+    // 클라이언트에서만 VConsole을 초기화
+    if (isClient && location.href.includes('#vc') && !vc.current) {
+      vc.current = new VConsole({ theme: 'dark', maxLogNumber: 1000 });
+    }
+  }, [isClient]);
+
+  return [vc.current] as const;
 }
