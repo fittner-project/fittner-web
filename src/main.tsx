d9677793170ba0da4ad559b0d85f04d728b53693
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-13 19:44:38 +0900

    [UPDATE] views=>pages 디렉토리 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import EntryPoint from "@/entryPoint";
 import router from "@/router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "@/styles/global.scss";
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
