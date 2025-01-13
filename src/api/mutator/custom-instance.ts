3a2049154cbb7b1ba1597a3eb9764a71576bbbb4
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-13 22:14:23 +0900

    [CREATE] axios 인스턴스 에러 처리

--- src/api/mutator/custom-instance.ts
+++ src/api/mutator/custom-instance.ts
@@ -10,3 +10,27 @@ export const customInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+customInstance.interceptors.response.use(
+  (response) => {
+    if (response.data && response.data.status === "FAIL") {
+      const alertMessage = response.data.errorMessage || response.data.message;
+      alert(alertMessage);
+
+      return Promise.reject(new Error(alertMessage));
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response?.data) {
+      const alertMessage =
+        error.response.data.errorMessage ||
+        error.response.data.message ||
+        "오류가 발생했습니다.";
+      alert(alertMessage);
+    } else {
+      alert(error.message || "오류가 발생했습니다.");
+    }
+    return Promise.reject(error);
+  }
+);
