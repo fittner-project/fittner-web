32e09239e4af1f30bc88994b1941c470dc033d44
이춘호 <lch92458@gmail.com>
2025-01-13 18:57:41 +0900

    [CREATE] 아이폰 se등 해상도 대응 hook

---
+++ src/hooks/common/useResolutionRatio.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+function useResolutionRatio(targetRatio = 0.47368421052) {
+  const [isBreakPoint, setIsBreakPoint] = useState(true);
+
+  useEffect(() => {
+    function updateVisibility() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const currentRatio = width / height;
+
+      setIsBreakPoint(currentRatio >= targetRatio);
+    }
+
+    updateVisibility();
+
+    window.addEventListener("resize", updateVisibility);
+    return () => window.removeEventListener("resize", updateVisibility);
+  }, [targetRatio]);
+
+  return isBreakPoint;
+}
+
+export default useResolutionRatio;
