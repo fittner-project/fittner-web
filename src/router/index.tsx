75c29c0e4ae60bb062247e3a9c010ce24349bd41
이춘호 <lch92458@gmail.com>
2025-01-13 18:58:09 +0900

    [FIX] Provider 적용 위치 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import RootLayout from "@/layout/default.tsx";
 import About from "@/views/About";
 import Home from "@/views/Home";
 import SignIn from "@/views/SignIn";
+import EntryPoint from "@/entryPoint";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter(
   [
     {
-      path: "",
-      element: <RootLayout />,
+      path: "/",
+      element: (
+        <EntryPoint>
+          <QueryClientProvider client={queryClient}>
+            <RootLayout />
+          </QueryClientProvider>
+        </EntryPoint>
+      ),
       children: [
         {
           path: "",
