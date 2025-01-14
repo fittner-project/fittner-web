670f64924c6d73831d70f1a0f73e1f810b87898a
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-15 01:14:11 +0900

    [CREATE] 공통 버튼 컴포넌트 구현

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,62 @@
+import { PropsWithChildren } from "react";
+
+interface ButtonProps {
+  backgroundColor: "primary_1" | "sub_1" | "grey_1";
+  disabled?: boolean;
+  fullWidth?: boolean;
+  className?: string;
+  onClick?: () => void;
+}
+
+function Button({
+  children,
+  backgroundColor,
+  fullWidth,
+  disabled,
+  className,
+  onClick,
+}: PropsWithChildren<ButtonProps>) {
+  const colorStyle = {
+    primary_1: {
+      backgroundColor: "#4C6AFF",
+      color: "#fff",
+    },
+    sub_1: {
+      backgroundColor: "#FF8194",
+      color: "#fff",
+    },
+    grey_1: {
+      backgroundColor: "#F2F4F6",
+      color: "#7F848D",
+    },
+  };
+
+  const currentStyle = disabled
+    ? colorStyle.grey_1
+    : colorStyle[backgroundColor];
+
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      style={{
+        background: currentStyle.backgroundColor,
+        color: currentStyle.color,
+        width: fullWidth ? "100%" : "auto",
+        borderRadius: "1.6rem",
+        height: "6rem",
+        fontFeatureSettings: "liga off, clig off",
+        fontFamily: "Pretendard",
+        fontSize: "2rem",
+        fontStyle: "normal",
+        fontWeight: "500",
+        lineHeight: "2.2rem",
+      }}
+      className={className}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
