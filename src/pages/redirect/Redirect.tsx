4e0bf29d0c8a1cc5db422cb840f71b6fbade1cb2
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-14 21:28:51 +0900

    [UPDATE] 애플 로그인 로직 수정

--- src/pages/redirect/Redirect.tsx
+++ src/pages/redirect/Redirect.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import styles from "./Redirect.module.scss";
@@ -8,20 +8,61 @@ import { useSocialAuth } from "../sign-in/hooks/useSocialAuth";
 
 function SocialCallback() {
   const [searchParams] = useSearchParams();
-  const state = searchParams.get("state");
-  const code = searchParams.get("code");
   const { handleSocialCallback } = useSocialAuth();
-  console.log("인가 code", code);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleAppleCallback = async (form: HTMLFormElement) => {
+    const formData = new FormData(form);
+    const appleCode = formData.get("code");
+    const appleState = formData.get("state");
+
+    if (appleCode && appleState) {
+      await handleSocialCallback({
+        socialType: appleState as SocialType,
+        code: appleCode as string,
+      });
+    }
+  };
+
+  const handleOtherSocialCallback = async (state: string, code: string) => {
+    await handleSocialCallback({
+      socialType: state as SocialType,
+      code,
+    });
+  };
 
   useEffect(() => {
-    if (state && code) {
-      handleSocialCallback({ socialType: state as SocialType, code });
-    }
-  }, [state, code, handleSocialCallback]);
+    const handleCallback = async () => {
+      try {
+        setIsProcessing(true);
+        const state = searchParams.get("state");
+        const code = searchParams.get("code");
+
+        if (location.pathname === "/redirect" && !code && !state) {
+          const form = document.querySelector("form");
+          if (!form) return;
+          await handleAppleCallback(form);
+          return;
+        }
+
+        if (state && code) {
+          await handleOtherSocialCallback(state, code);
+        }
+      } catch (error) {
+        console.error("Callback handling failed:", error);
+      } finally {
+        setIsProcessing(false);
+      }
+    };
+
+    handleCallback();
+  }, [searchParams, handleSocialCallback]);
 
   return (
     <div className={styles.container}>
-      <p className={styles.loading_text}>로그인 중...</p>
+      <p className={styles.loading_text}>
+        {isProcessing ? "로그인 중..." : "잠시만 기다려주세요..."}
+      </p>
     </div>
   );
 }
