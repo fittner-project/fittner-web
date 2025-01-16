a2eb6fb85788fa55679e06ed6225b7807997ab21
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-16 17:14:46 +0900

    [TEST] development, production 확인 콘솔

--- src/pages/sign-in/SignIn.tsx
+++ src/pages/sign-in/SignIn.tsx
@@ -9,6 +9,7 @@ import Image from "@/components/image/Image";
 
 function SignIn() {
   const { initSocialLogin } = useSocialAuth();
+  console.log("Current NODE_ENV:", process.env.NODE_ENV); // 환경 확인
 
   return (
     <BackgroundContainer>
