2bcb1ede17dd45dff74b4a93c15f8e372e5e356c
newy12 <newy12@naver.com>
2025-01-16 18:07:07 +0900

    #test. scope=email 삭제

--- src/pages/sign-in/hooks/useSocialAuth.ts
+++ src/pages/sign-in/hooks/useSocialAuth.ts
@@ -30,7 +30,7 @@ export const useSocialAuth = () => {
     const socialLoginUrls = {
       kakao: `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&state=${socialType}`,
       google: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email&state=${socialType}`,
-      apple: `https://appleid.apple.com/auth/authorize?client_id=${import.meta.env.VITE_APPLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&response_mode=query&scope=email&state=${socialType}`,
+      apple: `https://appleid.apple.com/auth/authorize?client_id=${import.meta.env.VITE_APPLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&response_mode=query&state=${socialType}`,
     };
 
     window.location.href = socialLoginUrls[socialType];
