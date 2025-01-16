83ea99c9d0680259a07325114a4d7ac130b8358e
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-01-16 15:43:35 +0900

    [CREATE] apple login 통신 테스트

--- src/pages/sign-in/hooks/useSocialAuth.ts
+++ src/pages/sign-in/hooks/useSocialAuth.ts
@@ -3,12 +3,24 @@ import { useLogin } from "@/api/generated/auth-controller/auth-controller";
 import { SocialType } from "@/auth/socialType";
 import { kakaoLoginService } from "@/auth/kakao";
 import { googleLoginService } from "@/auth/google";
-import { appleLoginService } from "@/auth/apple";
+
 import { openModal } from "@/utils/modal";
 import SignUpModal from "../components/sign-up-modal/SignUpModal";
+import { useAppleInfo } from "@/api/generated/권한/권한";
 
 export const useSocialAuth = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+
+  const { mutate: appleInfo } = useAppleInfo({
+    mutation: {
+      onSuccess: (data) => {
+        if (data.result?.userEmail) {
+          setEmail(data.result?.userEmail);
+        }
+      },
+    },
+  });
 
   const { mutate: login } = useLogin({
     mutation: {
@@ -30,7 +42,6 @@ export const useSocialAuth = () => {
       kakao: `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&state=${socialType}`,
       google: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email&state=${socialType}`,
       apple: `https://appleid.apple.com/auth/authorize?client_id=${import.meta.env.VITE_APPLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&response_mode=query&state=${socialType}`,
-
     };
 
     window.location.href = socialLoginUrls[socialType];
@@ -44,15 +55,13 @@ export const useSocialAuth = () => {
     code: string;
   }) => {
     try {
-      let email: string;
-
       switch (socialType) {
         case "kakao": {
           const accessToken = await kakaoLoginService.getToken({ code });
           const userInfo = await kakaoLoginService.getUserInfo({
             accessToken,
           });
-          email = userInfo.email;
+          setEmail(userInfo.email);
           break;
         }
         case "google": {
@@ -60,15 +69,14 @@ export const useSocialAuth = () => {
           const userInfo = await googleLoginService.getUserInfo({
             accessToken,
           });
-          email = userInfo.email;
+          setEmail(userInfo.email);
           break;
         }
         case "apple": {
-          const accessToken = await appleLoginService.getToken({ code });
-          const userInfo = await appleLoginService.getUserInfo({ accessToken });
-          email = userInfo.email;
+          appleInfo({ data: { code } });
           break;
         }
+
         default:
           throw new Error("Unknown social type");
       }
