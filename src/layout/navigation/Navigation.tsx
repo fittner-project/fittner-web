99570134ec9d5296690debb27df74a9f058ed66d
이춘호 <lch92458@gmail.com>
2025-01-16 10:44:51 +0900

    [CREATE] 공통 Layout 정의

---
+++ src/layout/navigation/Navigation.tsx
@@ -0,0 +1,5 @@
+import styles from "./Navigation.module.scss";
+
+export default function Navigation() {
+  return <div className={styles.wrapper}>Navigation</div>;
+}
---
+++ src/layout/sub-header/SubHeader.tsx
@@ -0,0 +1,5 @@
+import styles from "./SubHeader.module.scss";
+
+export default function SubHeader() {
+  return <div className={styles.wrapper}>SubHeader</div>;
+}
