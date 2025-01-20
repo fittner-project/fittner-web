19a8613da8d6824cf2e9659fa80c44b04020968f
이춘호 <lch92458@gmail.com>
2025-01-20 22:22:34 +0900

    [CREATE] MAIN페이지

--- src/layout/navigation/Navigation.tsx
+++ src/layout/navigation/Navigation.tsx
@@ -1,6 +1,15 @@
 import PATH from "@/router/path";
 import styles from "./Navigation.module.scss";
 import { useNavigate } from "react-router-dom";
+import Image from "@/components/image/Image";
+
+import {
+  nav_home,
+  nav_calendar,
+  nav_registration,
+  nav_sign,
+  nav_my,
+} from "@/assets/assets";
 
 type NavigationItem = {
   id: number;
@@ -12,31 +21,31 @@ const navigationItems: NavigationItem[] = [
   {
     id: 1,
     name: "홈",
-    icon: "",
+    icon: nav_home,
     path: PATH.HOME,
   },
   {
     id: 2,
     name: "나의수업",
-    icon: "",
+    icon: nav_calendar,
     path: "",
   },
   {
     id: 3,
     name: "등록",
-    icon: "",
+    icon: nav_registration,
     path: "",
   },
   {
     id: 4,
     name: "서명요청",
-    icon: "",
+    icon: nav_sign,
     path: "",
   },
   {
     id: 5,
     name: "My",
-    icon: "",
+    icon: nav_my,
     path: PATH.MY.DEFAULT,
   },
 ];
@@ -54,7 +63,9 @@ const NavigationItem = ({ item }: { item: NavigationItem }) => {
   const navigate = useNavigate();
   return (
     <div className={styles.item} onClick={() => navigate(item.path)}>
-      <div className={styles.ico}>{/* <Image src={item.icon} /> */}</div>
+      <div className={styles.ico}>
+        <Image src={item.icon} width={2.3} height={2.3} />
+      </div>
       <div className={styles.name}>{item.name}</div>
     </div>
   );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,7 +5,14 @@ import Image from "@/components/image/Image";
 import styles from "./Home.module.scss";
 import BackgroundContainer from "@/layout/containers/background-container/BackgroundContainer";
 import PaddingContainer from "@/layout/containers/padding-container/PaddingContainer";
-import { dumbbell } from "@/assets/assets";
+import {
+  chevronRight,
+  dumbbell,
+  main_calendar,
+  main_registration,
+  main_search,
+  main_sign,
+} from "@/assets/assets";
 import Training from "@/components/training/Training";
 import { MotionDiv } from "@/components/animation/Motion";
 
@@ -58,7 +65,10 @@ function Home() {
             initial={{ y: 10, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
-            달력달력
+            <div className={styles.calendar_title}>
+              12월 4주차 <Image src={chevronRight} width={2} height={2} />
+            </div>
+            <div className={styles.calendar}></div>
           </MotionDiv>
 
           <MotionDiv
@@ -67,7 +77,58 @@ function Home() {
             initial={{ y: 10, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
-            메뉴들
+            <div className={styles.my_class}>
+              <div className={styles.menu_title}>
+                나의 수업
+                <Image src={chevronRight} width={2} height={2} />
+              </div>
+              <div className={styles.menu_desc}>
+                나의 수업을 등록해 <br />
+                쉽게 관리해요!
+              </div>
+              <Image src={main_calendar} width={3.5} height={4} />
+            </div>
+
+            <div>
+              <div className={styles.registration}>
+                <Image src={main_registration} width={3.5} height={3.2} />
+                <div className={styles.menu_title}>
+                  회원 등록
+                  <Image src={chevronRight} width={2} height={2} />
+                </div>
+              </div>
+
+              <div className={styles.sign}>
+                <Image src={main_sign} width={4} height={4} />
+                <div className={styles.menu_title}>
+                  서명 요청
+                  <Image src={chevronRight} width={2} height={2} />
+                </div>
+              </div>
+            </div>
+          </MotionDiv>
+
+          <MotionDiv
+            className={styles.search}
+            transition={{ duration: 0.4, delay: 0.6 }}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+          >
+            <Image src={main_search} width={4.3} height={4.3} />
+            <div className={styles.search_content}>
+              <div className={styles.search_title}>회원정보</div>
+
+              <div className={styles.search_desc}>
+                내 회원 정보를 확인해보세요!
+              </div>
+            </div>
+
+            <Image
+              src={chevronRight}
+              width={2}
+              height={2}
+              className={styles.search_icon}
+            />
           </MotionDiv>
         </MotionDiv>
       </PaddingContainer>
