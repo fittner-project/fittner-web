76f466c4cbe51b6ebc75283197c2c6485cd15036
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-02-04 00:03:16 +0900

    [CREATE] 회원 탈퇴기능 구현

--- src/pages/my/default/components/account-actions/secession-bottom-sheet/SecessionBottomSheet.tsx
+++ src/pages/my/default/components/account-actions/secession-bottom-sheet/SecessionBottomSheet.tsx
@@ -2,8 +2,33 @@ import BottomSheet from "@/components/bottom-sheet/BottomSheet";
 import styles from "./SecessionBottomSheet.module.scss";
 import Button from "@/components/button/Button";
 import { closeBottomSheet } from "@/utils/bottomSheet";
+import useAuthStore from "@/store/auth";
+import { usePostUserDrop } from "@/api/generated/유저/유저";
+import PATH from "@/router/path";
+import { openModal } from "@/utils/modal";
+import SuccessModal from "@/components/modal/system-modal/success-modal/SuccessModal";
 
 export default function SecessionBottomSheet() {
+  const { logout } = useAuthStore();
+  const navigate = useNavigate();
+
+  const { mutate: postUserDrop } = usePostUserDrop({
+    mutation: {
+      onSuccess: () => {
+        openModal({
+          component: SuccessModal,
+          props: {
+            successMessage: "탈퇴가 \n 완료되었습니다.",
+            onCloseComplete: () => {
+              logout();
+              navigate(PATH.SIGN_IN);
+            },
+          },
+        });
+      },
+    },
+  });
+
   return (
     <BottomSheet>
       <div className={styles.container}>
@@ -18,7 +43,11 @@ export default function SecessionBottomSheet() {
           >
             취소
           </Button>
-          <Button backgroundColor="primary_1" fullWidth>
+          <Button
+            onClick={() => postUserDrop()}
+            backgroundColor="primary_1"
+            fullWidth
+          >
             탈퇴
           </Button>
         </div>
