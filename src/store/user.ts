9ae653b2e7a57c806c6f74a3c7ddfee33fbabf36
이춘호 <lch92458@gmail.com>
2025-02-05 12:07:22 +0900

    [CREATE] userStore에 Selected center 추가

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,10 +1,20 @@
-import { UserInfoResDto } from "@/api/generated/models";
+import { UserCenterListResDto, UserInfoResDto } from "@/api/generated/models";
 import { create } from "zustand";
 
-interface UserStore {
-  userInfo: UserInfoResDto;
+interface IUserDispatcher {
+  setSelectedCenter: (center: UserCenterListResDto) => void;
+  setUserInfo: (userInfo: UserInfoResDto) => void;
 }
+
+interface UserStore extends IUserDispatcher {
+  userInfo: UserInfoResDto;
+  selectedCenter: UserCenterListResDto;
+}
+
 export const useUserStore = create<UserStore>((set) => ({
   userInfo: {},
+  selectedCenter: {},
   setUserInfo: (userInfo: UserInfoResDto) => set({ userInfo }),
+  setSelectedCenter: (center: UserCenterListResDto) =>
+    set({ selectedCenter: center }),
 }));
