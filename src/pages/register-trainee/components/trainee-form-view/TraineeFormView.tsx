a64799e6d0e8c4eb30fd9aeee7813558f873ec50
이춘호 <lch92458@gmail.com>
2025-02-06 10:38:10 +0900

    [CREATE] 회원등록 완료

--- src/pages/register-trainee/components/trainee-form-view/TraineeFormView.tsx
+++ src/pages/register-trainee/components/trainee-form-view/TraineeFormView.tsx
@@ -3,6 +3,7 @@ import styles from "./TraineeFormView.module.scss";
 import { RegisterTraineeForm } from "../../RegisterTrainee";
 import Input from "@/components/input/Input";
 import Radio from "@/components/radio/Radio";
+import Button from "@/components/button/Button";
 
 interface ITraineeFormViewProps {
   form: UseFormReturn<RegisterTraineeForm, any, undefined>;
@@ -52,6 +53,44 @@ export default function TraineeFormView({ form }: ITraineeFormViewProps) {
             {...form.register("memberGender", { required: true })}
           />
         </div>
+      </div>
+
+      <div className={styles.field}>
+        <p className={styles.title}>생년월일</p>
+        <Input
+          inputType="line"
+          maxLength={6}
+          className={styles.name_input}
+          {...form.register("memberBirth", {
+            required: true,
+          })}
+          placeholder="YYMMDD (6자리)"
+        />
+      </div>
+
+      <div className={styles.field}>
+        <p className={styles.title}>주소</p>
+        <Input
+          inputType="line"
+          maxLength={50}
+          className={styles.name_input}
+          {...form.register("memberAddress", {
+            required: true,
+          })}
+          placeholder="회원의 주소를 입력해주세요"
+        />
+      </div>
+
+      <div className={styles.button_container}>
+        <Button
+          type="submit"
+          backgroundColor="primary_1"
+          fullWidth
+          className={styles.next_button}
+          disabled={false}
+        >
+          다음
+        </Button>
       </div>
     </div>
   );
