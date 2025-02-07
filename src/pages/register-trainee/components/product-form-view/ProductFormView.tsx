0146ab0ff531d8825806553b43977bf9314c9a1d
이춘호 <lch92458@gmail.com>
2025-02-07 11:41:53 +0900

    [CREATE] React-Mobile-Picker 임시 적용

--- src/pages/register-trainee/components/product-form-view/ProductFormView.tsx
+++ src/pages/register-trainee/components/product-form-view/ProductFormView.tsx
@@ -3,64 +3,108 @@ import styles from "./ProductFormView.module.scss";
 import { RegisterTraineeForm } from "../../RegisterTrainee";
 import Input from "@/components/input/Input";
 import Button from "@/components/button/Button";
+import Picker from "react-mobile-picker";
 
 interface IProductFormViewProps {
   form: UseFormReturn<RegisterTraineeForm, any, undefined>;
 }
 
+const selections = {
+  title: ["Mr.", "Mrs.", "Ms.", "Dr."],
+  firstName: ["John", "Micheal", "Elizabeth"],
+  lastName: ["Lennon", "Jackson", "Jordan", "Legend", "Taylor"],
+};
+
 export default function ProductFormView({ form }: IProductFormViewProps) {
+  const [pickerValue, setPickerValue] = useState({
+    title: "Mr.",
+    firstName: "Micheal",
+    lastName: "Jordan",
+  });
   return (
     <div className={styles.container}>
       <div className={styles.field}>
-        <p className={styles.title}>이름</p>
+        <p className={styles.title}>상품명</p>
         <Input
           inputType="line"
-          maxLength={5}
+          maxLength={30}
           className={styles.name_input}
-          {...form.register("memberName", {
+          {...form.register("productName", {
             required: true,
           })}
-          placeholder="회원의 이름을 입력해주세요"
+          placeholder="상품명을 입력해주세요"
         />
       </div>
 
       <div className={styles.field}>
-        <p className={styles.title}>연락처</p>
+        <p className={styles.title}>기간</p>
+        <div className={styles.period}>ㅁㄴㅇㅁㄴㅇㅁㄴ</div>
+        <Picker value={pickerValue} onChange={setPickerValue}>
+          {Object.keys(selections).map((name) => (
+            <Picker.Column key={name} name={name}>
+              {selections[name as keyof typeof selections].map((option) => (
+                <Picker.Item key={option} value={option}>
+                  {option}
+                </Picker.Item>
+              ))}
+            </Picker.Column>
+          ))}
+        </Picker>
+      </div>
+
+      <div className={styles.field}>
+        <p className={styles.title}>수업횟수</p>
         <Input
           inputType="line"
-          maxLength={11}
+          maxLength={3}
           className={styles.name_input}
-          {...form.register("memberPhone", {
+          {...form.register("productCount", {
             required: true,
           })}
-          placeholder="회원의 연락처를 입력해주세요"
+          placeholder="수업횟수를 입력해주세요"
+          type="number"
         />
       </div>
 
       <div className={styles.field}>
-        <p className={styles.title}>생년월일</p>
+        <p className={styles.title}>금액</p>
         <Input
           inputType="line"
-          maxLength={6}
+          maxLength={10}
           className={styles.name_input}
-          {...form.register("memberBirth", {
+          {...form.register("productPrice", {
             required: true,
           })}
-          placeholder="YYMMDD (6자리)"
+          placeholder="금액을 입력해주세요"
+          type="number"
         />
       </div>
 
       <div className={styles.field}>
-        <p className={styles.title}>주소</p>
+        <p className={styles.title}>메모</p>
         <Input
           inputType="line"
-          maxLength={50}
+          maxLength={100}
           className={styles.name_input}
-          {...form.register("memberAddress", {
+          {...form.register("memberMemo", {
             required: true,
           })}
-          placeholder="회원의 주소를 입력해주세요"
+          placeholder="회원 메모를 입력해주세요"
         />
+      </div>
+
+      <div className={styles.field}>
+        <p className={styles.title}>가입 경로</p>
+        <Input
+          inputType="line"
+          maxLength={0}
+          className={styles.name_input}
+          {...form.register("memberJoinPath", {
+            required: true,
+          })}
+          placeholder="가입 경로를 선택해주세요"
+        />
+        {/* Input에 endAdorment 추가 */}
       </div>
 
       <div className={styles.button_container}>
