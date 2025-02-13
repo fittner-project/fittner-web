5b444c0bcd370d3a707178b8d657983632bff45f
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-02-13 21:32:30 +0900

    [CREATE] RevenueDetail 퍼블리싱

--- src/pages/my/revenue-detail/RevenueDetail.tsx
+++ src/pages/my/revenue-detail/RevenueDetail.tsx
@@ -1,5 +1,8 @@
 import dayjs from "dayjs";
 import Revenue from "../components/revenue/Revenue";
+import styles from "./RevenueDetail.module.scss";
+import { chevronRightGrey } from "@/assets/assets";
+import Image from "@/components/image/Image";
 
 export default function RevenueDetail() {
   const generateDateArray = () => {
@@ -20,12 +23,38 @@ export default function RevenueDetail() {
   );
 
   return (
-    <div>
+    <div className={styles.container}>
       <Revenue
         type="detail"
         dateArray={dateArray}
         setActiveDate={setActiveDate}
       />
+
+      <div className={styles.trainee_list}>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div key={index} className={styles.trainee}>
+            <div className={styles.trainee_left}>
+              <p className={styles.trainee_name}>김영재 회원님</p>
+              <p className={styles.item_type}>프리미엄PT20</p>
+              <p className={styles.trainee_date}>2024.10.01-2025.01.01</p>
+            </div>
+            <div className={styles.trainee_right}>
+              <div className={styles.round}>
+                <p className={styles.round_date}>이번 달</p>
+                <div className={styles.round_count}>3회차</div>
+                <Image
+                  src={chevronRightGrey}
+                  alt="chevronRightGrey"
+                  width={2.8}
+                  height={2.8}
+                />
+              </div>
+
+              <p className={styles.price}>{"150000".toLocaleString()}원</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
