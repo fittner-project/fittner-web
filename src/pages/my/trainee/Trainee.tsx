7830161ca1248d6b87829d604d07f0326604d8b9
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-02-17 21:44:33 +0900

    [CREATE] 회원 유저 스켈레톤 적용

--- src/pages/my/trainee/Trainee.tsx
+++ src/pages/my/trainee/Trainee.tsx
@@ -7,9 +7,10 @@ import { chevronRightGrey, userProfile } from "@/assets/assets";
 import { useSearch } from "@/hooks/useSearch";
 import { useForm } from "react-hook-form";
 import classNames from "classnames";
+import Skeleton from "@/components/skeleton/Skeleton";
 
 export default function Trainee() {
-  const { data: traineeData } = useGetUserMembers();
+  const { data: traineeData, isLoading } = useGetUserMembers();
   const trainees = traineeData?.result;
   const { register, watch } = useForm();
   const { filteredData } = useSearch({
@@ -18,7 +19,6 @@ export default function Trainee() {
     searchFields: ["memberName", "memberPhone"],
   });
   const searchValue = watch("searchValue");
-  console.log(filteredData);
 
   return (
     <PaddingContainer>
@@ -29,41 +29,110 @@ export default function Trainee() {
           style={{ height: "4.8rem" }}
           {...register("searchValue")}
         />
+        {isLoading ? (
+          <Skeleton
+            width={10}
+            height={2.2}
+            borderRadius={1}
+            style={{ marginTop: "2.6rem", marginBottom: "0.9rem" }}
+          />
+        ) : (
+          <div className={styles.trainee_count}>
+            {!searchValue && <p>전체</p>}
+            <p>{filteredData?.length}명</p>
+          </div>
+        )}
 
-        <div className={styles.trainee_count}>
-          {!searchValue && <p>전체</p>}
-          <p>{filteredData?.length}명</p>
-        </div>
-
-        <div className={styles.scroll_container}>
-          {filteredData?.map((trainee) => (
-            <div key={trainee.memberId} className={styles.trainee_item}>
-              <section className={styles.left_section}>
-                <div
-                  className={classNames(styles.trainee_profile, {
-                    [styles.male]: trainee.memberGender === "M",
-                    [styles.female]: trainee.memberGender === "F",
-                  })}
+        <div
+          className={classNames(styles.scroll_container, {
+            [styles.loading_finish]: !isLoading,
+          })}
+        >
+          {isLoading
+            ? Array.from({ length: 10 }).map((_, index) => (
+                <Skeleton
+                  key={index}
+                  style={{
+                    height: "9.5rem",
+                    margin: "0 -1.8rem",
+                    padding: "2rem 1.8rem",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
                 >
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "1.4rem",
+                    }}
+                  >
+                    <Skeleton
+                      width={5.3}
+                      height={5.3}
+                      borderRadius={1000}
+                      backgroundColor="skeleton_2"
+                    />
+                    <div>
+                      <Skeleton
+                        width={10}
+                        height={2.2}
+                        borderRadius={1}
+                        backgroundColor="skeleton_2"
+                      />
+                      <Skeleton
+                        width={17}
+                        height={2.2}
+                        borderRadius={1}
+                        backgroundColor="skeleton_2"
+                        style={{ marginTop: "0.6rem" }}
+                      />
+                    </div>
+                  </div>
+                  <Skeleton
+                    width={2.8}
+                    height={2.8}
+                    borderRadius={1}
+                    backgroundColor="skeleton_2"
+                  />
+                </Skeleton>
+              ))
+            : filteredData?.map((trainee) => (
+                <div key={trainee.memberId} className={styles.trainee_item}>
+                  <section className={styles.left_section}>
+                    <div
+                      className={classNames(styles.trainee_profile, {
+                        [styles.male]: trainee.memberGender === "M",
+                        [styles.female]: trainee.memberGender === "F",
+                      })}
+                    >
+                      <Image
+                        src={userProfile}
+                        alt="프로필 이미지"
+                        width={3}
+                        height={3}
+                      />
+                    </div>
+                    <div className={styles.trainee_info}>
+                      <p className={styles.trainee_name}>
+                        {trainee.memberName}
+                      </p>
+                      <div className={styles.trainee_info_detail}>
+                        <p>{trainee.memberPhone}</p>
+                        <p>만 {trainee.memberAge}세</p>
+                      </div>
+                    </div>
+                  </section>
+
                   <Image
-                    src={userProfile}
-                    alt="프로필 이미지"
-                    width={3}
-                    height={3}
+                    src={chevronRightGrey}
+                    alt=""
+                    width={2.8}
+                    height={2.8}
                   />
                 </div>
-                <div className={styles.trainee_info}>
-                  <p className={styles.trainee_name}>{trainee.memberName}</p>
-                  <div className={styles.trainee_info_detail}>
-                    <p>{trainee.memberPhone}</p>
-                    <p>만 {trainee.memberAge}세</p>
-                  </div>
-                </div>
-              </section>
-
-              <Image src={chevronRightGrey} alt="" width={2.8} height={2.8} />
-            </div>
-          ))}
+              ))}
         </div>
       </div>
     </PaddingContainer>
