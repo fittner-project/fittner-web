e5c52c6408a48d02f0e6204817b337eb1c30cdf3
이춘호 <lch92458@gmail.com>
2025-02-20 18:14:16 +0900

    [CREATE] 회원정보 확인 바텀시트

--- src/pages/register-trainee/components/trainee-confirm-bottom-sheet/TraineeConfirmBottomSheet.tsx
+++ src/pages/register-trainee/components/trainee-confirm-bottom-sheet/TraineeConfirmBottomSheet.tsx
@@ -1,28 +1,77 @@
 import BottomSheet from "@/components/bottom-sheet/BottomSheet";
 import styles from "./TraineeConfirmBottomSheet.module.scss";
-import { closeBottomSheet } from "@/utils/bottomSheet";
+import { closeBottomSheet, openBottomSheet } from "@/utils/bottomSheet";
 import { UseFormReturn } from "react-hook-form";
 import { RegisterTraineeForm } from "../../RegisterTrainee";
+import Row from "@/components/flex/Row";
+import Button from "@/components/button/Button";
+import ProductConfirmBottomSheet from "../product-confirm-bottom-sheet/ProductConfirmBottomSheet";
+import { WaitForSeconds } from "@/utils/coroutine";
 
 interface TraineeConfirmBottomSheetProps {
   form: UseFormReturn<RegisterTraineeForm, any, undefined>;
-  onClickBtnTwo: () => void;
 }
 
 export default function TraineeConfirmBottomSheet({
   form,
-  onClickBtnTwo,
 }: TraineeConfirmBottomSheetProps) {
-  console.log(form.getValues());
-
   return (
     <BottomSheet>
       <div className={styles.container}>
         <div className={styles.title}>
-          입력하신 <span>회원 정보</span>를<br /> 확인해주세요.
+          입력하신 <span>회원정보</span>를<br /> 확인해주세요.
         </div>
 
-        <div className={styles.content}></div>
+        <div className={styles.content}>
+          <Row className={styles.content_item} justifyContent="space-between">
+            <div>이름</div>
+            <div>{form.getValues().memberName}</div>
+          </Row>
+          <Row className={styles.content_item} justifyContent="space-between">
+            <div>연락처</div>
+            <div>{form.getValues().memberPhone}</div>
+          </Row>
+          <Row className={styles.content_item} justifyContent="space-between">
+            <div>성별</div>
+            <div>{form.getValues().memberGender}</div>
+          </Row>
+          <Row className={styles.content_item} justifyContent="space-between">
+            <div>생년월일</div>
+            <div>{form.getValues().memberBirth}</div>
+          </Row>
+          <Row className={styles.content_item} justifyContent="space-between">
+            <div>주소</div>
+            <div>{form.getValues().memberAddress}</div>
+          </Row>
+        </div>
+
+        <Row className={styles.button_container} justifyContent="space-between">
+          <Button
+            backgroundColor="grey_1"
+            fullWidth
+            className={styles.next_button}
+            onClick={() => closeBottomSheet()}
+          >
+            수정
+          </Button>
+
+          <Button
+            backgroundColor="primary_1"
+            fullWidth
+            className={styles.next_button}
+            onClick={() => {
+              closeBottomSheet();
+              WaitForSeconds(300).then(() => {
+                openBottomSheet({
+                  component: ProductConfirmBottomSheet,
+                  props: { form },
+                });
+              });
+            }}
+          >
+            다음
+          </Button>
+        </Row>
       </div>
     </BottomSheet>
   );
