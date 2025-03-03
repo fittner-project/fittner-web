c7275bde6ccaf93f6fa6232b703775d0db6f33d1
이춘호 <lch92458@gmail.com>
2025-03-03 21:05:52 +0900

    [CREATE] EntryPoint에서 트레이너 수업 일정이랑 컬러 관리

--- src/entryPoint.tsx
+++ src/entryPoint.tsx
@@ -5,6 +5,10 @@ import PATH from "./router/path";
 import useSplash from "./hooks/useSplash";
 import Image from "./components/image/Image";
 import { useGetUserInfo } from "./api/generated/유저/유저";
+import {
+  useGetUserReservationColors,
+  useGetUserReservations,
+} from "./api/generated/수업/수업";
 
 interface IProps {
   children: ReactNode;
@@ -41,7 +45,20 @@ const Authorized = ({ children }: IProps) => {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const setUserInfo = useUserStore((state) => state.setUserInfo);
   const setSelectedCenter = useUserStore((state) => state.setSelectedCenter);
+  const setReservationColors = useUserStore(
+    (state) => state.setReservationColors
+  );
+  const setCurrentMonthReservations = useUserStore(
+    (state) => state.setCurrentMonthReservations
+  );
   const { data } = useGetUserInfo({ query: { enabled: !!isAuthenticated } });
+  const { data: reservations } = useGetUserReservations({
+    //@ts-ignore
+    //서버에서 스웨거 타입 바꿔줘야함 타입에러나는데 정상적인 폼의 요청이라 200떨어짐
+    reservationStartDate: "2024-01-01",
+    reservationEndDate: "2024-12-31",
+  });
+  const { data: reservationColors } = useGetUserReservationColors();
 
   useEffect(() => {
     if (data) {
@@ -49,6 +66,18 @@ const Authorized = ({ children }: IProps) => {
       setSelectedCenter(data.result?.centerInfo?.[0] || {});
     }
   }, [data]);
+
+  useEffect(() => {
+    if (reservationColors) {
+      setReservationColors(reservationColors.result || {});
+    }
+  }, [reservationColors]);
+
+  useEffect(() => {
+    if (reservations) {
+      setCurrentMonthReservations(reservations.result || []);
+    }
+  }, [reservations]);
   //인증이 된 이후 앱 전체 적용 로직들
 
   //브랜드별 컬러 API 작업이 끝나면 인증 후 여기서 받은 뒤 zustand에 저장하고 사용
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,4 +1,9 @@
-import { CenterInfo, DefaultInfo } from "@/api/generated/models";
+import {
+  CenterInfo,
+  DefaultInfo,
+  GroupedReservationMemberDto,
+  ReservationColorResDto,
+} from "@/api/generated/models";
 import { storageKeys } from "@/constants/storageKeys";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
@@ -6,11 +11,17 @@ import { createJSONStorage, persist } from "zustand/middleware";
 interface IUserDispatcher {
   setUserInfo: (userInfo: DefaultInfo) => void;
   setSelectedCenter: (center: CenterInfo) => void;
+  setReservationColors: (reservationColors: ReservationColorResDto) => void;
+  setCurrentMonthReservations: (
+    reservations: GroupedReservationMemberDto[]
+  ) => void;
 }
 
 interface UserStore extends IUserDispatcher {
   userInfo: DefaultInfo;
   selectedCenter: CenterInfo;
+  reservationColors: ReservationColorResDto;
+  currentMonthreservations: GroupedReservationMemberDto[];
 }
 
 export const useUserStore = create(
@@ -18,9 +29,16 @@ export const useUserStore = create(
     (set, _get) => ({
       userInfo: {},
       selectedCenter: {},
+      reservationColors: {},
+      currentMonthreservations: [],
       setUserInfo: (userInfo: DefaultInfo) => set({ userInfo }),
       setSelectedCenter: (center: CenterInfo) =>
         set({ selectedCenter: center }),
+      setReservationColors: (reservationColors: ReservationColorResDto) =>
+        set({ reservationColors }),
+      setCurrentMonthReservations: (
+        reservations: GroupedReservationMemberDto[]
+      ) => set({ currentMonthreservations: reservations }),
     }),
     {
       name: storageKeys.user,
