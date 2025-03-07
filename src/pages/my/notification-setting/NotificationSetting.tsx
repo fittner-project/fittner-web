603af108b7ce1101f7825c905fac6d1552dd5317
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-03-07 16:37:25 +0900

    [CREATE] 푸시메세지 스켈레톤 구현

--- src/pages/my/notification-setting/NotificationSetting.tsx
+++ src/pages/my/notification-setting/NotificationSetting.tsx
@@ -2,10 +2,11 @@ import PaddingContainer from "@/layout/containers/padding-container/PaddingConta
 import SwitchSection from "./components/SwitchSection";
 import styles from "./NotificationSetting.module.scss";
 import { useGetUserPushs } from "@/api/generated/푸시/푸시";
+import Skeleton from "@/components/skeleton/Skeleton";
 
 export default function NotificationSetting() {
   const centerId = useUserStore((state) => state.selectedCenter.centerId);
-  const { data: userPushData } = useGetUserPushs(
+  const { data: userPushData, isLoading } = useGetUserPushs(
     {
       centerId: centerId || "",
     },
@@ -17,9 +18,36 @@ export default function NotificationSetting() {
   return (
     <PaddingContainer>
       <div className={styles.container}>
-        {userPushsData?.map((notification) => (
-          <SwitchSection notification={notification} />
-        ))}
+        {isLoading
+          ? Array.from({ length: 10 }).map((_, index) => (
+              <Skeleton
+                key={index}
+                height={6.8}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  margin: "0 -1.8rem",
+                  padding: "0 1.8rem",
+                }}
+              >
+                <Skeleton
+                  width={8}
+                  height={3}
+                  borderRadius={1}
+                  backgroundColor="skeleton_2"
+                />
+                <Skeleton
+                  width={5}
+                  height={3}
+                  borderRadius={1000}
+                  backgroundColor="skeleton_2"
+                />
+              </Skeleton>
+            ))
+          : userPushsData?.map((notification) => (
+              <SwitchSection notification={notification} />
+            ))}
       </div>
     </PaddingContainer>
   );
