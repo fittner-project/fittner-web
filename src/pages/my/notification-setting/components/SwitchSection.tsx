817a34c6642f3bd8331e2542b283f0a81aaf96de
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-03-08 04:04:58 +0900

    [CREATE] 푸시 알림 설정 api 통신

--- src/pages/my/notification-setting/components/SwitchSection.tsx
+++ src/pages/my/notification-setting/components/SwitchSection.tsx
@@ -1,16 +1,70 @@
 import Switch from "@/components/switch/Switch";
 import styles from "./SwitchSection.module.scss";
 import { PushSetResDto } from "@/api/generated/models";
+import {
+  getGetUserMyPagePushQueryKey,
+  usePostUserMyPagePushSet,
+} from "@/api/generated/마이페이지/마이페이지";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface SwitchSectionProps {
   notification: PushSetResDto;
 }
 
 export default function SwitchSection({ notification }: SwitchSectionProps) {
+  const queryClient = useQueryClient();
+  const { mutate: changePush } = usePostUserMyPagePushSet({
+    mutation: {
+      onMutate: async (newData) => {
+        await queryClient.cancelQueries({
+          queryKey: getGetUserMyPagePushQueryKey(),
+        });
+
+        const previousData = queryClient.getQueryData(
+          getGetUserMyPagePushQueryKey()
+        );
+
+        queryClient.setQueryData(
+          getGetUserMyPagePushQueryKey(),
+          (old: any) => ({
+            ...old,
+            result: old.result.map((item: PushSetResDto) =>
+              item.pushKind === newData.data.pushKind
+                ? { ...item, pushSetYn: newData.data.pushSetYn }
+                : item
+            ),
+          })
+        );
+
+        return { previousData };
+      },
+      onError: (err, newData, context: any) => {
+        queryClient.setQueryData(
+          getGetUserMyPagePushQueryKey(),
+          context.previousData
+        );
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries({
+          queryKey: getGetUserMyPagePushQueryKey(),
+        });
+      },
+    },
+  });
   return (
     <div className={styles.container}>
       <p className={styles.title}>{notification.pushKind}</p>
-      <Switch isSwitchOn={notification.pushSetYn === "Y"} onChange={() => {}} />
+      <Switch
+        isSwitchOn={notification.pushSetYn === "Y"}
+        onChange={() => {
+          changePush({
+            data: {
+              pushKind: notification.pushKind || "",
+              pushSetYn: notification.pushSetYn === "Y" ? "N" : "Y",
+            },
+          });
+        }}
+      />
     </div>
   );
 }
