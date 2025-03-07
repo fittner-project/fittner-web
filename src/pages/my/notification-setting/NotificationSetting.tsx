550a646fad7afab01f88f58f605dcf45424e7b1e
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-03-07 16:29:14 +0900

    [CREATE] 푸시메세지 조회 api 통신

--- src/pages/my/notification-setting/NotificationSetting.tsx
+++ src/pages/my/notification-setting/NotificationSetting.tsx
@@ -5,35 +5,19 @@ import { useGetUserPushs } from "@/api/generated/푸시/푸시";
 
 export default function NotificationSetting() {
   const centerId = useUserStore((state) => state.selectedCenter.centerId);
-  const { data: userPushs } = useGetUserPushs(
+  const { data: userPushData } = useGetUserPushs(
     {
       centerId: centerId || "",
-      currentPageNo: "1",
-      recordsPerPage: "10",
     },
     { query: { enabled: !!centerId } }
   );
 
-  console.log(userPushs);
-  const notificationSettingArr = [
-    {
-      title: "일반 알림",
-      isSwitchOn: true,
-    },
-    {
-      title: "수업 스케쥴 알림",
-      isSwitchOn: true,
-    },
-    {
-      title: "마케팅 알림",
-      isSwitchOn: true,
-    },
-  ];
+  const userPushsData = userPushData?.result;
 
   return (
     <PaddingContainer>
       <div className={styles.container}>
-        {notificationSettingArr.map((notification) => (
+        {userPushsData?.map((notification) => (
           <SwitchSection notification={notification} />
         ))}
       </div>
--- src/pages/my/notification-setting/components/SwitchSection.tsx
+++ src/pages/my/notification-setting/components/SwitchSection.tsx
@@ -1,18 +1,19 @@
 import Switch from "@/components/switch/Switch";
 import styles from "./SwitchSection.module.scss";
+import { PushResDto } from "@/api/generated/models";
 
 interface SwitchSectionProps {
-  notification: {
-    title: string;
-    isSwitchOn: boolean;
-  };
+  notification: PushResDto;
 }
 
 export default function SwitchSection({ notification }: SwitchSectionProps) {
   return (
     <div className={styles.container}>
-      <p className={styles.title}>{notification.title}</p>
-      <Switch isSwitchOn={notification.isSwitchOn} onChange={() => {}} />
+      <p className={styles.title}>{notification.pushTitle}</p>
+      <Switch
+        isSwitchOn={notification.pushReadYn === "Y"}
+        onChange={() => {}}
+      />
     </div>
   );
 }
