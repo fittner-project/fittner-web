3ca12c0c4ba13c4295a78feb9441f6ca5d7c9d5b
이춘호 <lch92458@gmail.com>
2025-03-11 17:24:01 +0900

    [CREATE] 나의 수업 -> 기본 full calendar 연결

--- src/pages/my-lessons/MyLessons.tsx
+++ src/pages/my-lessons/MyLessons.tsx
@@ -1,10 +1,37 @@
 import PaddingContainer from "@/layout/containers/padding-container/PaddingContainer";
 import styles from "./MyLessons.module.scss";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { useCallback } from "react";
 
 export default function MyLessons() {
+  const handleDatesSet = useCallback((arg: any) => {
+    console.log("현재 보여지는 달:", arg.view.title);
+  }, []);
+
   return (
     <PaddingContainer>
-      <div className={styles.container}>dd</div>
+      <div className={styles.container}>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          locale="ko" // 한글 설정
+          headerToolbar={{
+            start: "prev",
+            center: "title",
+            end: "next",
+          }}
+          titleFormat={{
+            year: "numeric",
+            month: "long",
+          }}
+          height="auto"
+          datesSet={handleDatesSet}
+          dayHeaderFormat={{
+            weekday: "short",
+          }}
+        />
+      </div>
     </PaddingContainer>
   );
 }
