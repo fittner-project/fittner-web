6c5de6cf9f5e39b5cab684c7b705ab42dbbd3584
이춘호 <lch92458@gmail.com>
2025-05-08 17:08:33 +0900

    [CREATE] tsx에서도 사용할 수 있도록 colors.ts 생성

---
+++ src/constants/colors.ts
@@ -0,0 +1,23 @@
+const COLORS = {
+  // 무채색 계열
+  grey_1: "var(--grey-type-a, #f2f4f6)",
+  grey_2: "var(--grey-type-b, #b0b8c1)",
+  grey_3: "var(--grey-type-c, #7f848d)",
+  grey_4: "var(--grey-type-d, #4d5662)",
+
+  // 브랜드 컬러
+  primary_1: "var(--primary, #4c6aff)",
+  sub_1: "var(--sub, #ff8194)",
+
+  // 텍스트 컬러
+  text_1: "var(--text-type-a, #191f28)",
+  text_2: "var(--text-type-b, #ffffff)",
+  text_3: "var(--text-type-c, #b0b8c1)",
+  text_4: "var(--text-type-d, #7f848d)",
+  text_5: "var(--text-type-e, #4d5662)",
+  text_6: "var(--text-type-f, #4c6aff)",
+  text_7: "var(--text-type-g, #ff8194)",
+} as const;
+
+export type ColorKey = keyof typeof COLORS;
+export default COLORS;
