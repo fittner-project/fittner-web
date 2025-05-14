10811a6d650bbdad1b1663a412141f31dbb63916
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-05-14 17:52:37 +0900

    [CREATE] 회원 삭제 기능 구현

--- src/pages/my/trainee/components/trainee-ticket-modal/trainee-ticket-setting-bottom-sheet/delete-trainee-modal/DeleteTraineeModal.tsx
+++ src/pages/my/trainee/components/trainee-ticket-modal/trainee-ticket-setting-bottom-sheet/delete-trainee-modal/DeleteTraineeModal.tsx
@@ -1,6 +1,14 @@
 import Modal from "@/components/modal/Modal";
 import styles from "./DeleteTraineeModal.module.scss";
 import Input from "@/components/input/Input";
+import { useForm } from "react-hook-form";
+import {
+  getGetUserMembersQueryKey,
+  useDeleteUser,
+} from "@/api/generated/유저/유저";
+import { closeModal, openModal } from "@/utils/modal";
+import SuccessModal from "@/components/modal/system-modal/success-modal/SuccessModal";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface DeleteTraineeModalProps {
   memberId: string;
@@ -11,6 +19,34 @@ export default function DeleteTraineeModal({
   memberId,
   memberName,
 }: DeleteTraineeModalProps) {
+  const { register, watch } = useForm();
+  const memberNameValue = watch("memberName");
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteUser } = useDeleteUser({
+    mutation: {
+      onSuccess: () => {
+        closeModal();
+        queryClient.invalidateQueries({
+          queryKey: getGetUserMembersQueryKey(),
+        });
+        openModal({
+          component: SuccessModal,
+          props: {
+            onCloseComplete: () => closeModal(),
+            successMessage: "회원 삭제가 \n 완료되었습니다.",
+          },
+        });
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (memberName === memberNameValue) {
+      deleteUser({ memberId });
+    }
+  }, [memberNameValue]);
+
   return (
     <Modal>
       <div className={styles.container}>
@@ -18,7 +54,12 @@ export default function DeleteTraineeModal({
           확인을 위해 회원님의 <br />
           이름을 작성해주세요
         </p>
-        <Input autoFocus inputType="line" placeholder={memberName} />
+        <Input
+          autoFocus
+          inputType="line"
+          placeholder={memberName}
+          {...register("memberName")}
+        />
       </div>
     </Modal>
   );
