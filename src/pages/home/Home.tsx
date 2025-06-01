fb5a0efe6972a09931df61a0da0e2507359d02e6
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-06-01 16:29:40 +0900

    [UPDATE] 홈 레이아웃 컨테이너 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,7 +6,7 @@ import useCalendarStore from "@/store/calendar";
 
 import styles from "./Home.module.scss";
 import BackgroundContainer from "@/layout/containers/background-container/BackgroundContainer";
-import PaddingContainer from "@/layout/containers/padding-container/PaddingContainer";
+
 import {
   chevronRightGrey,
   dumbbell,
@@ -49,124 +49,122 @@ function Home() {
 
   return (
     <BackgroundContainer>
-      <PaddingContainer>
-        <MotionDiv className={styles.container}>
-          <MotionDiv
-            className={styles.dumbbell_container}
-            transition={{ duration: 0.4, delay: 1 }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-          >
-            <Image
-              src={dumbbell}
-              width={13.3}
-              height={19}
-              className={styles.dumbbell}
-            />
-          </MotionDiv>
+      <MotionDiv className={styles.container}>
+        <MotionDiv
+          className={styles.dumbbell_container}
+          transition={{ duration: 0.4, delay: 1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <Image
+            src={dumbbell}
+            width={13.3}
+            height={19}
+            className={styles.dumbbell}
+          />
+        </MotionDiv>
 
-          <MotionDiv
-            className={styles.class_info}
-            transition={{ duration: 1 }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
+        <MotionDiv
+          className={styles.class_info}
+          transition={{ duration: 1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <div className={styles.day}>{today}</div>
+          <div className={styles.guide}>
+            {!nearestLesson ? (
+              <>
+                <span>등록된 수업</span>이 없어요!
+              </>
+            ) : (
+              <>
+                <span>{nearestLesson.ptnCnt}번째 </span>&nbsp; 수업이에요!
+              </>
+            )}
+          </div>
+
+          <Training lesson={nearestLesson} />
+        </MotionDiv>
+
+        <MotionDiv
+          className={styles.calendar_container}
+          transition={{ duration: 0.4, delay: 0.6 }}
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <WeeklyCalendar />
+        </MotionDiv>
+
+        <MotionDiv
+          className={styles.menu_container}
+          transition={{ duration: 0.4, delay: 0.6 }}
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <div
+            className={styles.my_class}
+            onClick={() => navigate(PATH.MY_LESSONS)}
           >
-            <div className={styles.day}>{today}</div>
-            <div className={styles.guide}>
-              {!nearestLesson ? (
-                <>
-                  <span>등록된 수업</span>이 없어요!
-                </>
-              ) : (
-                <>
-                  <span>{nearestLesson.ptnCnt}번째 </span>&nbsp; 수업이에요!
-                </>
-              )}
+            <div className={styles.menu_title}>
+              나의 수업
+              <Image src={chevronRightGrey} width={2} height={2} />
             </div>
+            <div className={styles.menu_desc}>
+              나의 수업을 등록해 <br />
+              쉽게 관리해요!
+            </div>
+            <Image src={main_calendar} width={3.5} height={4} />
+          </div>
 
-            <Training lesson={nearestLesson} />
-          </MotionDiv>
-
-          <MotionDiv
-            className={styles.calendar_container}
-            transition={{ duration: 0.4, delay: 0.6 }}
-            initial={{ y: 10, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            <WeeklyCalendar />
-          </MotionDiv>
-
-          <MotionDiv
-            className={styles.menu_container}
-            transition={{ duration: 0.4, delay: 0.6 }}
-            initial={{ y: 10, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
+          <div className={styles.registration_sign_container}>
             <div
-              className={styles.my_class}
-              onClick={() => navigate(PATH.MY_LESSONS)}
+              onClick={() => navigate(PATH.REGISTER_TRAINEE)}
+              className={styles.registration}
             >
+              <Image src={main_registration} width={3.5} height={3.2} />
               <div className={styles.menu_title}>
-                나의 수업
+                회원 등록
                 <Image src={chevronRightGrey} width={2} height={2} />
               </div>
-              <div className={styles.menu_desc}>
-                나의 수업을 등록해 <br />
-                쉽게 관리해요!
-              </div>
-              <Image src={main_calendar} width={3.5} height={4} />
             </div>
 
-            <div className={styles.registration_sign_container}>
-              <div
-                onClick={() => navigate(PATH.REGISTER_TRAINEE)}
-                className={styles.registration}
-              >
-                <Image src={main_registration} width={3.5} height={3.2} />
-                <div className={styles.menu_title}>
-                  회원 등록
-                  <Image src={chevronRightGrey} width={2} height={2} />
-                </div>
-              </div>
-
-              <div
-                onClick={() => navigate(PATH.SIGNATURE.LIST)}
-                className={styles.sign}
-              >
-                <Image src={main_sign} width={4} height={4} />
-                <div className={styles.menu_title}>
-                  서명 요청
-                  <Image src={chevronRightGrey} width={2} height={2} />
-                </div>
+            <div
+              onClick={() => navigate(PATH.SIGNATURE.LIST)}
+              className={styles.sign}
+            >
+              <Image src={main_sign} width={4} height={4} />
+              <div className={styles.menu_title}>
+                서명 요청
+                <Image src={chevronRightGrey} width={2} height={2} />
               </div>
             </div>
-          </MotionDiv>
-
-          <MotionDiv
-            className={styles.search}
-            transition={{ duration: 0.4, delay: 0.6 }}
-            initial={{ y: 10, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            onClick={() => navigate(PATH.MY.TRAINEE)}
-          >
-            <Image src={main_search} width={4.3} height={4.3} />
-            <div className={styles.search_content}>
-              <div className={styles.search_title}>회원정보</div>
-
-              <div className={styles.search_desc}>
-                내 회원 정보를 확인해보세요!
-              </div>
-            </div>
-
-            <Image
-              src={chevronRightGrey}
-              width={2}
-              height={2}
-              className={styles.search_icon}
-            />
-          </MotionDiv>
+          </div>
         </MotionDiv>
-      </PaddingContainer>
+
+        <MotionDiv
+          className={styles.search}
+          transition={{ duration: 0.4, delay: 0.6 }}
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          onClick={() => navigate(PATH.MY.TRAINEE)}
+        >
+          <Image src={main_search} width={4.3} height={4.3} />
+          <div className={styles.search_content}>
+            <div className={styles.search_title}>회원정보</div>
+
+            <div className={styles.search_desc}>
+              내 회원 정보를 확인해보세요!
+            </div>
+          </div>
+
+          <Image
+            src={chevronRightGrey}
+            width={2}
+            height={2}
+            className={styles.search_icon}
+          />
+        </MotionDiv>
+      </MotionDiv>
     </BackgroundContainer>
   );
 }
