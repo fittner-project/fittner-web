d848c7ffdf91bde150ce58aed46ee37ef5fe5641
youngjae <newy12@naver.com>
2025-11-08 20:46:35 +0900

    refactor: Remove React.StrictMode to prevent double rendering in dev

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,5 @@ import router from "@/router";
 import "@/styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
